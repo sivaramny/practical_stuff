cc2897fc60d81f2a849fad34b1cc922d8318c718
sivaram <sivaram.nyayapati@gmail.com>
2015-03-12 16:23:34 +0530

    Changed the title

--- node-test-app/routes/index.js
+++ node-test-app/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'TeamF1 Networks Pvt Ltd' });
+  res.render('index', { title: 'Sivaram' });
 });
 
 module.exports = router;
